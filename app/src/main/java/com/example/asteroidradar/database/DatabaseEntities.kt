28e9dada7a06ee9cd65e97b4be7431a702225c61
samue <samuebrandenburg12@gmail.com>
2021-08-11 19:39:35 -0500

    * removed redundant code

--- app/src/main/java/com/example/asteroidradar/database/DatabaseEntities.kt
+++ app/src/main/java/com/example/asteroidradar/database/DatabaseEntities.kt
@@ -9,17 +9,11 @@ import androidx.room.PrimaryKey
 data class Asteroid(
     @PrimaryKey(autoGenerate = false)
     var id: Long= 0L,
-    @ColumnInfo(name = "approach_date")
     var approach_date: Long = 0L,
-    @ColumnInfo(name = "absolute_magnitude")
     var absolute_magnitude:Double = 0.0,
-    @ColumnInfo(name = "estimated_diameter_max")
     var estimated_diameter_max:Double = 0.0,
-    @ColumnInfo(name = "is_potentialy_hazardous_asteroid")
     var hazardous: Boolean = false,
-    @ColumnInfo(name = "kilometers_per_second")
     var kilometers_per_second:Double = 0.0,
-    @ColumnInfo(name = "astronomical_value")
     var astronomical_value:Double= 0.0)
 
 
--- app/src/main/java/com/example/asteroidradar/network/NearEarthObjects.kt
+++ app/src/main/java/com/example/asteroidradar/network/NearEarthObjects.kt
@@ -10,29 +10,6 @@ data class DataTransferObject(
     var isHazardous: Boolean?,
     var approachDate: String?,
     val kilometersPerSecond: Double? = -1.0,
-    val astronomical: Double? = -1.0,
-
-//
+    val astronomical: Double? = -1.0
 )
 
-
-data class Asteroid(
-    var id: Long,
-    var absoluteMagnitude: Double,
-    var estimatedDiameters: EstimatedDiameters,
-    var closeApproachData: CloseApproachData,
-    var isHazardous: Boolean,
-) {
-}
-
-data class EstimatedDiameters(val estimatedDiameterMax: Double)
-
-
-data class CloseApproachData(
-    val date: String,
-    var relativeVelocity: RelativeVelocity,
-    var missDistance: MissDistance
-)
-
-data class RelativeVelocity(val kilometersPerSecond: Double)
-data class MissDistance(val astronomical: Double)
